a4ad14e34240cd9aa11caeadb22ab9099e976365
williamunoz <williamunoz@gmail.com>
2019-01-29 23:38:29 -0500

    Create LaminarID2.m

---
+++ FunctionDev/LaminarID2.m
@@ -0,0 +1,17 @@
+basePath = pwd;
+
+[baseFolder,baseName] = fileparts(basePath);
+savefile = fullfile(basePath,[baseName,'.',num2str(i),'.LayerID.lfp.mat']);
+
+[lof,lospec,t_lo,hif,hispec,t_hi,MUA] = bz_MUAGammafromDat(basePath,'channels',i);
+
+%% Saving laminar pspectrum
+LayerID.lof = lof;
+LayerID.lospec = lospec;
+LayerID.t_lo = t_lo;
+LayerID.hif = hif;
+LayerID.hispec = hispec;
+LayerID.t_hi = t_hi;
+LayerID.MUA = MUA;
+
+save(savefile,'LayerID');
