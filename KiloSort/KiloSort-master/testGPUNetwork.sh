e9bd83cb53cc57a98bbcd9c6a7e8a2dfe871b6aa
williamunoz <williamunoz@gmail.com>
2019-02-05 20:39:27 -0500

    Updates to KilosortMaster

--- KiloSort/KiloSort-master/testGPUNetwork.sh
+++
@@ -1,7 +0,0 @@
-
-me="$(whoami)"
-horus=$(ssh $me@horus  "bash /home/buzadmin/scripts/testGPUsforMATLAB.sh")
-isi=$(ssh buzadmin@hyperion  "bash testGPUsforMATLAB.sh")
-buzmonster=$(bash /home/buzadmin/Scripts/testGPUsforMATLAB.sh)
-
-id -u -n
