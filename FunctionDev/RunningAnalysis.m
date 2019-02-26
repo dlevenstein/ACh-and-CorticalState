f5f1050164bf7ae1579fb54d8437cfbd860c9dc2
williamunoz <williamunoz@gmail.com>
2019-02-26 18:48:59 -0500

    Updates to run analysis on cluster

---
+++ FunctionDev/RunningAnalysis.m
@@ -0,0 +1,4 @@
+bz_RunAnalysis('PupilWhiskAnalysis');
+bz_RunAnalysis('LFPSlopeAndWhiskAnalysis');
+bz_RunAnalysis('LFPSlopeAndPupilAnalysis');
+bz_RunAnalysis('LFPSpecByPupilAnalysis');
--- FunctionDev/bz_RunAnalysis.m
+++ FunctionDev/bz_RunAnalysis.m
@@ -1,4 +1,4 @@
-function [  ] = bz_RunAnalysis(analysisfunction,datasetfolder,varargin)
+function [  ] = bz_RunAnalysis(analysisfunction,varargin)
 %bz_RunAnalysis(analysisfunction,datasetfolder) is a high level function that
 %takes as its input an analysis function and returns the result of the
 %analysis on multiple recordings
@@ -31,14 +31,16 @@ addParameter(p,'cluster',false,@islogical)
 parse(p,varargin{:})
 savein = p.Results.savein;
 cluster = p.Results.cluster;
-%% Select Recordings to Analyze 
+%% Select Recordings to Analyze
 
-[possiblebasePaths,possiblebaseNames] = bz_FindBasePaths(datasetfolder);
-[s,v] = listdlg('PromptString','Which recording(s) would you like analyze?',...
-                'ListString',possiblebaseNames);
-baseName = possiblebaseNames(s);
-basePath = possiblebasePaths(s); 
+% [possiblebasePaths,possiblebaseNames] = bz_FindBasePaths(datasetfolder);
+% [s,v] = listdlg('PromptString','Which recording(s) would you like analyze?',...
+%                 'ListString',possiblebaseNames);
+% baseName = possiblebaseNames(s);
+% basePath = possiblebasePaths(s);
 
+basePath = pwd;
+[baseFolder,baseName] = fileparts(basePath);
 
 %How many results are in the analysis function?
 numresults = nargout(analysisfunction);
@@ -53,34 +55,38 @@ end
 
 %% Loop through the recordings and run the analysis
 
-numrecs = length(baseName);
-display(['Running Analysis on Recordings (',num2str(numrecs),')'])
-for rr = 1:numrecs
-    display(['Recording: ',num2str(rr),' of ',num2str(numrecs),' - ',baseName{rr}])
+%numrecs = length(baseName);
+%display(['Running Analysis on Recordings (',num2str(numrecs),')'])
+%for rr = 1:numrecs
+
+    %display(['Recording: ',num2str(rr),' of ',num2str(numrecs),' - ',baseName{rr}])
     
     [inputNames, outputNames] = get_arg_names(fullfile(functionpath,[analysisfunction,'.m']));
     outputNames = outputNames{1}; inputNames = inputNames{1};
     
-    [results{1:numresults}] = feval(analysisfunction,basePath{rr},figfolder);
-        
+    %[results{1:numresults}] = feval(analysisfunction,basePath{rr},figfolder);
+    [results{1:numresults}] = feval(analysisfunction,basePath,figfolder);
+    
     for ss = 1:numresults
         eval([outputNames{ss} '= results{ss};']);
     end
     
     %Add: save date, baseName
-    switch savein
-        case 'basePath'
-            savefolder = basePath{rr}; %or basePath...
-        case 'functionPath'
-            savefolder = figfolder; %or basePath...
-        otherwise
-            error('savein must be ''basePath'' or ''functionPath''')
-    end
-    savename = fullfile(savefolder,[baseName{rr},'.AnalysisResults.',analysisfunction,'.mat']);
+%     switch savein
+%         case 'basePath'
+%             %savefolder = basePath{rr}; %or basePath...
+%             savefolder = basePath; %or basePath...
+%         case 'functionPath'
+%             savefolder = figfolder; %or basePath...
+%         otherwise
+%             error('savein must be ''basePath'' or ''functionPath''')
+%     end
+    %savename = fullfile(savefolder,[baseName{rr},'.AnalysisResults.',analysisfunction,'.mat']);
+    savename = fullfile(basePath,[baseName,'.AnalysisResults.',analysisfunction,'.mat']);
     save(savename,outputNames{:});
     
     close all
-end
+%end
 
 disp('ANALYSIS COMPLETE. HUZZAH!')
 
