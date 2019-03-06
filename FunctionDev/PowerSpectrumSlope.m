ec8e48bcd777b519a8dab8b63da337ebd43c0207
williamunoz <williamunoz@gmail.com>
2019-03-06 17:13:06 -0500

    Update PowerSpectrumSlope.m

--- FunctionDev/PowerSpectrumSlope.m
+++ FunctionDev/PowerSpectrumSlope.m
@@ -42,6 +42,7 @@ EMGwhisk.EMGsm = EMGwhisk.EMGsm(spontidx);
 %% Optimizing lower frequency bounds and window/dt
 % Loading LFP for SlowWave detection channel
 load(fullfile(basePath,[baseName,'.SlowWaves.events.mat']),'SlowWaves');
+%SlowWaves = bz_LoadEvents(basePath,'SlowWaves');
 channel = SlowWaves.detectorinfo.detectionchannel;
 clear SlowWaves;
 
@@ -52,6 +53,7 @@ lfp.data = lfp.data(spontidx);
 lfp.timestamps = lfp.timestamps(spontidx);
 
 downsamplefactor = 3;
+%lfp = bz_DownsampleLFP(lfp,downsamplefactor);
 lfp.samplingRate = lfp.samplingRate./downsamplefactor;
 lfp.data = downsample(double(lfp.data),downsamplefactor);
 lfp.timestamps = downsample(lfp.timestamps,downsamplefactor);
@@ -71,19 +73,25 @@ Fracrsqcorr = zeros(length(lowerbound),size(movingwin,1));
 Fracrsqp = zeros(length(lowerbound),size(movingwin,1));
 
 for x = 1:size(movingwin,1)
+    
     nwin = floor((length(lfp.data) - movingwin(x,1))/movingwin(x,2));
     sig = zeros(movingwin(x,1),nwin);
-    
+    timestamp = zeros(nwin,1);
     for i = 1 : nwin
-        sig(:,i) = lfp.data(ceil((i-1)*movingwin(x,2))+1 : ceil((i-1)*movingwin(x,2))+movingwin(x,1));
+        idx = [ceil((i-1)*movingwin(x,2))+1 : ceil((i-1)*movingwin(x,2))+movingwin(x,1)];
+        sig(:,i) = lfp.data(idx);
+        %figure out timestamp associated with window i 
+        timestamp(i) = mean(lfp.timestamps(idx));
     end
     
     Frac = amri_sig_fractal_gpu(sig,srate,'detrend',1);
-    st = movingwin(x,1)/(srate*2);
-    Frac.timestamps = st + ((0:nwin-1) * movingwin(x,2)/srate);
+    %st = movingwin(x,1)/(srate*2);
+    %Frac.timestamps = st + ((0:nwin-1) * movingwin(x,2)/srate);
+    Frac.timestamps = timestamp;
     
     % Interpolating...
-    PSS.EMG = interp1(EMGwhisk.timestamps,EMGwhisk.EMGenvelope,Frac.timestamps);
+    PSS.EMG = interp1(EMGwhisk.timestamps,EMGwhisk.EMGenvelope,...
+        Frac.timestamps,'nearest');
     PSS.pupilsize = interp1(pupildilation.timestamps,pupildilation.data,...
         Frac.timestamps,'nearest');
     
@@ -179,36 +187,46 @@ title('Frac-RSQ correlation');
 NiceSave('Frac_EMG_Pupil_RSQ_Corr_p_win_lof',figfolder,baseName);
 
 %% Example slow/fast PSS, RSQ, EMG and Pupil...
-% Fast PSS
-movingwin = round([1 0.25].*srate);
-Frange = [5,100];
-
-nwin = floor((length(lfp.data) - movingwin(1))/movingwin(2));
-sig = zeros(movingwin(1),nwin);
-for i = 1 : nwin
-    sig(:,i) = lfp.data(ceil((i-1)*movingwin(2))+1 : ceil((i-1)*movingwin(2))+movingwin(1));
-end
-Frac = amri_sig_fractal_gpu(sig,srate,'detrend',1);
-Frac.timestamps = st + ((0:nwin-1) * movingwin(2)/srate);
-Frac_fast = amri_sig_plawfit(Frac,Frange);
-
 % Slow PSS
 movingwin = round([15 3.75].*srate);
 Frange = [2.5,100];
 
 nwin = floor((length(lfp.data) - movingwin(1))/movingwin(2));
 sig = zeros(movingwin(1),nwin);
+timestamp = zeros(nwin,1);
 for i = 1 : nwin
-    sig(:,i) = lfp.data(ceil((i-1)*movingwin(2))+1 : ceil((i-1)*movingwin(2))+movingwin(1));
+    idx = [ceil((i-1)*movingwin(2))+1 : ceil((i-1)*movingwin(2))+movingwin(1)];
+    sig(:,i) = lfp.data(idx);
+    %figure out timestamp associated with window i
+    timestamp(i) = mean(lfp.timestamps(idx));
 end
-Frac = amri_sig_fractal_gpu(sig,srate,'detrend',1);
-Frac.timestamps = st + ((0:nwin-1) * movingwin(2)/srate);
-Frac_slow = amri_sig_plawfit(Frac,Frange);
+Frac_slow = amri_sig_fractal_gpu(sig,srate,'detrend',1);
+Frac_slow.timestamps = timestamp;
+Frac_slow = amri_sig_plawfit(Frac_slow,Frange);
 
-PSS.EMG = interp1(EMGwhisk.timestamps,EMGwhisk.EMGenvelope,Frac_slow.timestamps);
+% Fast PSS
+movingwin = round([1 0.25].*srate);
+Frange = [5,100];
+
+nwin = floor((length(lfp.data) - movingwin(1))/movingwin(2));
+sig = zeros(movingwin(1),nwin);
+timestamp = zeros(nwin,1);
+for i = 1 : nwin
+    idx = [ceil((i-1)*movingwin(2))+1 : ceil((i-1)*movingwin(2))+movingwin(1)];
+    sig(:,i) = lfp.data(idx);
+    %figure out timestamp associated with window i
+    timestamp(i) = mean(lfp.timestamps(idx));
+end
+Frac_fast = amri_sig_fractal_gpu(sig,srate,'detrend',1);
+Frac_fast.timestamps = timestamp;
+Frac_fast = amri_sig_plawfit(Frac_fast,Frange);
+
+% Interpolating...
+PSS.EMG = interp1(EMGwhisk.timestamps,EMGwhisk.EMGenvelope,...
+    Frac_fast.timestamps,'nearest');
 PSS.pupilsize = interp1(pupildilation.timestamps,pupildilation.data,...
-    Frac_slow.timestamps,'nearest');
-    
+    Frac_fast.timestamps,'nearest');
+
 %% FIGURE
 figure;
 
@@ -224,27 +242,22 @@ plot(Frac_slow.timestamps,Frac_slow.Beta.*-1,'r');
 legend({'Frac fast','Frac slow'},'location','southeast')
 
 h(3) = subplot(9,1,4:5); hold on;
-plot(Frac_slow.timestamps,PSS.EMG,'k');
+plot(Frac_fast.timestamps,PSS.EMG,'k');
 legend({'EMG'},'location','southeast')
 
 h(4) = subplot(9,1,6:7); hold on;
-plot(Frac_slow.timestamps,PSS.pupilsize,'k');
+plot(Frac_fast.timestamps,PSS.pupilsize,'k');
 legend({'Pupil diameter'},'location','southeast')
 
 h(5) = subplot(9,1,8:9); hold on;
-deltarange = find(Frac_slow.freq >= 0.5 & Frac_slow.freq <= 3);
-plot(Frac_slow.timestamps,...
-    nanmean(Frac_slow.osci(deltarange,:),1)./max(nanmean(Frac_slow.osci(deltarange,:),1)),'r');
-thetarange = find(Frac_slow.freq >= 4 & Frac_slow.freq <= 10);
-plot(Frac_slow.timestamps,...
-    nanmean(Frac_slow.osci(thetarange,:),1)./max(nanmean(Frac_slow.osci(thetarange,:),1)),'g');
-gammarange = find(Frac_slow.freq >= 40 & Frac_slow.freq <= 120);
-plot(Frac_slow.timestamps,...
-    nanmean(Frac_slow.osci(gammarange,:),1)./max(nanmean(Frac_slow.osci(gammarange,:),1)),'b');
-legend({'Delta','Theta','Gamma'},'location','southeast')
+imagesc(Frac_fast.timestamps,Frac_fast.freq,Frac_fast.osci);
+caxis([min(min(Frac_fast.osci)) max(max(Frac_fast.osci))])
+ylabel('f (hz)'); ylim([1 120]);
+colormap(gca,'jet');
+legend({'Oscillatory PSpec'},'location','southeast')
 
 linkaxes(h,'x');
-xlim([250 1050]);
+xlim([250 650]);
 xlabel('time (s)');
 
 NiceSave('Example_PSS_rsq_osci_behavior',figfolder,baseName);
@@ -256,19 +269,22 @@ usechannels(ismember(usechannels,badchannels))=[];
 
 % Assuming that LFP still remains loaded from prior analysis...
 movingwin = round([1 0.25].*srate);
-nwin = floor((length(lfp.data) - movingwin(1))/movingwin(2));
-st = movingwin(1)/(srate*2);
 
+nwin = floor((length(lfp.data) - movingwin(1))/movingwin(2));
 sig = zeros(movingwin(1),nwin);
+timestamp = zeros(nwin,1);
 for i = 1 : nwin
-    sig(:,i) = lfp.data(ceil((i-1)*movingwin(2))+1 : ceil((i-1)*movingwin(2))+movingwin(1));
+    idx = [ceil((i-1)*movingwin(2))+1 : ceil((i-1)*movingwin(2))+movingwin(1)];
+    sig(:,i) = lfp.data(idx);
+    %figure out timestamp associated with window i
+    timestamp(i) = mean(lfp.timestamps(idx));
 end
 clear lfp
 
 Frac = amri_sig_fractal_gpu(sig,srate,'detrend',1);
-Frac.timestamps = st + ((0:nwin-1) * movingwin(2)/srate);
+Frac.timestamps = timestamp;
 Frac = amri_sig_plawfit(Frac,Frange);
-    
+
 % Selected from prior analysis
 Frange = [5, 100]; % define frequency range for power-law fitting
 
@@ -279,6 +295,21 @@ PSScorr.EMG = zeros(length(usechannels),1);
 PSScorr.EMG_p = zeros(length(usechannels),1);
 PSScorr.Pup = zeros(length(usechannels),1);
 PSScorr.Pup_p = zeros(length(usechannels),1);
+
+dcorr.EMG = zeros(length(usechannels),1);
+dcorr.EMG_p = zeros(length(usechannels),1);
+dcorr.Pup = zeros(length(usechannels),1);
+dcorr.Pup_p = zeros(length(usechannels),1);
+
+tcorr.EMG = zeros(length(usechannels),1);
+tcorr.EMG_p = zeros(length(usechannels),1);
+tcorr.Pup = zeros(length(usechannels),1);
+tcorr.Pup_p = zeros(length(usechannels),1);
+
+gcorr.EMG = zeros(length(usechannels),1);
+gcorr.EMG_p = zeros(length(usechannels),1);
+gcorr.Pup = zeros(length(usechannels),1);
+gcorr.Pup_p = zeros(length(usechannels),1);
 
 Oscicorr.EMG = zeros(size(Frac.osci,1),length(usechannels));
 Oscicorr.EMG_p = zeros(size(Frac.osci,1),length(usechannels));
@@ -303,13 +334,17 @@ for cc = 1:length(usechannels)
     
     %% Deconstruction
     sig = zeros(movingwin(1),nwin);
+    timestamp = zeros(nwin,1);
     for i = 1 : nwin
-        sig(:,i) = lfp.data(ceil((i-1)*movingwin(2))+1 : ceil((i-1)*movingwin(2))+movingwin(1));
+        idx = [ceil((i-1)*movingwin(2))+1 : ceil((i-1)*movingwin(2))+movingwin(1)];
+        sig(:,i) = lfp.data(idx);
+        %figure out timestamp associated with window i
+        timestamp(i) = mean(lfp.timestamps(idx));
     end
     clear lfp
     
     Frac = amri_sig_fractal_gpu(sig,srate,'detrend',1);
-    Frac.timestamps = st + ((0:nwin-1) * movingwin(2)/srate);
+    Frac.timestamps = timestamp;
     Frac = amri_sig_plawfit(Frac,Frange);
     
     PSS = cat(2,PSS,Frac.Beta.*-1);
@@ -332,6 +367,30 @@ for cc = 1:length(usechannels)
         corr(log10(temp_Pup)',Frac.Beta.*-1,...
         'type','spearman','rows','complete');
     
+    deltaidx = find(Frac.freq >= 0.5 & Frac.freq <= 3);
+    [dcorr.EMG(cc),dcorr.EMG_p(cc)] =...
+        corr(log10(temp_EMG)',(nanmean(Frac.osci(deltaidx,:),1))',...
+        'type','spearman','rows','complete');
+    [dcorr.Pup(cc),dcorr.Pup_p(cc)] =...
+        corr(log10(temp_Pup)',(nanmean(Frac.osci(deltaidx,:),1))',...
+        'type','spearman','rows','complete');
+    
+    thetaidx = find(Frac.freq >= 4 & Frac.freq <= 10);
+    [tcorr.EMG(cc),tcorr.EMG_p(cc)] =...
+        corr(log10(temp_EMG)',(nanmean(Frac.osci(thetaidx,:),1))',...
+        'type','spearman','rows','complete');
+    [tcorr.Pup(cc),tcorr.Pup_p(cc)] =...
+        corr(log10(temp_Pup)',(nanmean(Frac.osci(thetaidx,:),1))',...
+        'type','spearman','rows','complete');
+    
+    gammaidx = find(Frac.freq >= 40 & Frac.freq <= 120);
+    [gcorr.EMG(cc),gcorr.EMG_p(cc)] =...
+        corr(log10(temp_EMG)',(nanmean(Frac.osci(gammaidx,:),1))',...
+        'type','spearman','rows','complete');
+    [gcorr.Pup(cc),gcorr.Pup_p(cc)] =...
+        corr(log10(temp_Pup)',(nanmean(Frac.osci(gammaidx,:),1))',...
+        'type','spearman','rows','complete');
+    
     for x = 1:size(Frac.osci,1)
         [Oscicorr.EMG(x,cc),Oscicorr.EMG_p(x,cc)] =...
             corr(log10(temp_EMG)',Frac.osci(x,:)',...
@@ -340,11 +399,12 @@ for cc = 1:length(usechannels)
             corr(log10(temp_Pup)',Frac.osci(x,:)',...
             'type','spearman','rows','complete');
     end
+    
 end
 
 %% FIGURE
 figure;
-subplot(2,2,1);
+subplot(2,3,1);
 imagesc(PSSstatsdepth.bins,1:length(usechannels),PSSstatsdepth.dist)
 ColorbarWithAxis([min(min(PSSstatsdepth.dist)) max(max(PSSstatsdepth.dist))],['counts'])
 caxis([min(min(PSSstatsdepth.dist)) max(max(PSSstatsdepth.dist))])
@@ -354,7 +414,7 @@ ylabel('channel no. (depth-aligned)')
 colormap(gca,'jet')
 axis tight
 
-subplot(2,2,2);
+subplot(2,3,2);
 plot(PSScorr.EMG,1:length(usechannels),'k','linewidth',2)
 hold on;
 plot(PSScorr.Pup,1:length(usechannels),'r','linewidth',2)
@@ -363,7 +423,7 @@ xlabel('PSS-Behavior correlation');
 set(gca,'ydir','reverse')
 axis tight
 
-subplot(2,2,3);
+subplot(2,3,4);
 imagesc(log10(Frac.freq),1:length(usechannels),Oscicorr.EMG')
 LogScale('x',10);
 colormap(gca,'jet')
@@ -372,7 +432,7 @@ caxis([min(min(Oscicorr.EMG)) max(max(Oscicorr.EMG))])
 xlabel('f (Hz)'); ylabel('channel no. (depth-aligned)');
 title('Oscillatory LFP-EMG correlation');
 
-subplot(2,2,4);
+subplot(2,3,5);
 imagesc(log10(Frac.freq),1:length(usechannels),Oscicorr.Pup')
 LogScale('x',10);
 colormap(gca,'jet')
@@ -380,6 +440,26 @@ ColorbarWithAxis([min(min(Oscicorr.Pup)) max(max(Oscicorr.Pup))],['Spearman corr
 caxis([min(min(Oscicorr.Pup)) max(max(Oscicorr.Pup))])
 xlabel('f (Hz)'); ylabel('channel no. (depth-aligned)');
 title('Oscillatory LFP-Pupil diameter correlation');
+
+subplot(2,6,5);
+plot(dcorr.EMG,1:length(usechannels),'r','linewidth',2)
+hold on;
+plot(tcorr.EMG,1:length(usechannels),'r','linewidth',2)
+plot(gcorr.EMG,1:length(usechannels),'r','linewidth',2)
+legend('d','t','g','location','southeast')
+xlabel('Oscillatory-EMG correlation');
+set(gca,'ydir','reverse')
+axis tight
+
+subplot(2,6,6);
+plot(dcorr.Pup,1:length(usechannels),'r','linewidth',2)
+hold on;
+plot(tcorr.Pup,1:length(usechannels),'r','linewidth',2)
+plot(gcorr.Pup,1:length(usechannels),'r','linewidth',2)
+legend('d','t','g','location','southeast')
+xlabel('Oscillatory-Pupil diameter correlation');
+set(gca,'ydir','reverse')
+axis tight
 
 NiceSave('fPSS_Behavior_CorrbyDepth',figfolder,baseName)
 
@@ -437,17 +517,20 @@ lfp.data = downsample(double(lfp.data),downsamplefactor);
 lfp.timestamps = downsample(lfp.timestamps,downsamplefactor);
 
 movingwin = round([15 3.75].*srate);
-nwin = floor((length(lfp.data) - movingwin(1))/movingwin(2));
-st = movingwin(1)/(srate*2);
 
+nwin = floor((length(lfp.data) - movingwin(1))/movingwin(2));
 sig = zeros(movingwin(1),nwin);
+timestamp = zeros(nwin,1);
 for i = 1 : nwin
-    sig(:,i) = lfp.data(ceil((i-1)*movingwin(2))+1 : ceil((i-1)*movingwin(2))+movingwin(1));
+    idx = [ceil((i-1)*movingwin(2))+1 : ceil((i-1)*movingwin(2))+movingwin(1)];
+    sig(:,i) = lfp.data(idx);
+    %figure out timestamp associated with window i
+    timestamp(i) = mean(lfp.timestamps(idx));
 end
 clear lfp
 
 Frac = amri_sig_fractal_gpu(sig,srate,'detrend',1);
-Frac.timestamps = st + ((0:nwin-1) * movingwin(2)/srate);
+Frac.timestamps = timestamp;
 Frac = amri_sig_plawfit(Frac,Frange);
 
 % Selected from prior analysis
@@ -484,13 +567,17 @@ for cc = 1:length(usechannels)
     
     %% Deconstruction
     sig = zeros(movingwin(1),nwin);
+    timestamp = zeros(nwin,1);
     for i = 1 : nwin
-        sig(:,i) = lfp.data(ceil((i-1)*movingwin(2))+1 : ceil((i-1)*movingwin(2))+movingwin(1));
+        idx = [ceil((i-1)*movingwin(2))+1 : ceil((i-1)*movingwin(2))+movingwin(1)];
+        sig(:,i) = lfp.data(idx);
+        %figure out timestamp associated with window i
+        timestamp(i) = mean(lfp.timestamps(idx));
     end
     clear lfp
     
     Frac = amri_sig_fractal_gpu(sig,srate,'detrend',1);
-    Frac.timestamps = st + ((0:nwin-1) * movingwin(2)/srate);
+    Frac.timestamps = timestamp;
     Frac = amri_sig_plawfit(Frac,Frange);
     
     PSS = cat(2,PSS,Frac.Beta.*-1);
