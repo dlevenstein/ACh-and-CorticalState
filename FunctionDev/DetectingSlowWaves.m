6e125cc787d4d3370f87b64b42e08d455d89e580
williamunoz <williamunoz@gmail.com>
2019-03-06 12:49:49 -0500

    Update DetectingSlowWaves.m

--- FunctionDev/DetectingSlowWaves.m
+++ FunctionDev/DetectingSlowWaves.m
@@ -12,11 +12,17 @@ load(fullfile(basePath,[baseName,'.sessionInfo.mat']));
 dtchan = sessionInfo.slowdetectorchannel;
 
 %%
+spikes = bz_GetSpikes('noPrompts',true);
+
+%%
 mergefile = fullfile(basePath,[baseName,'.MergePoints.events.mat']);
 load(mergefile,'MergePoints');
 
 spontidx = find(startsWith(MergePoints.foldernames,"Spont"));
 sponttimes = [MergePoints.timestamps(spontidx(1),1) MergePoints.timestamps(spontidx(end),2)];
 
+% DetectSlowWaves(basePath,'NREMInts',sponttimes,'CTXChans',dtchan,'filterparms',...
+%     filterparms,'noSpikes',true,'showFig',true,'forceReload',true,'noPrompts',true);
+
 DetectSlowWaves(basePath,'NREMInts',sponttimes,'CTXChans',dtchan,'filterparms',...
-    filterparms,'noSpikes',true,'showFig',true,'forceReload',true,'noPrompts',true);
+    filterparms,'spikes',spikes,'showFig',true,'forceReload',true,'noPrompts',true);
