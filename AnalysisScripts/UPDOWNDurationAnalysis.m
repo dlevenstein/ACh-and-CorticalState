9c6c778221d568221058074b4737873d16a075e5
dlevenstein <dan.levenstein@gmail.com>
2019-05-15 18:39:50 -0400

    UPDOWNDur analysis

--- AnalysisScripts/UPDOWNDurationAnalysis.m
+++ AnalysisScripts/UPDOWNDurationAnalysis.m
@@ -49,77 +49,188 @@ pupilcycle.highpup = log10(pupilcycle.amp)>pupilcycle.pupthresh;
 % EMG
 EMGwhisk = bz_LoadBehavior(basePath,'EMGwhisk');
 
+%% Get rid of recording start/stop artifact and restrict to spontaneous behavior
 
+%Specifying SPONT whisking
+load(fullfile(basePath,[baseName,'.MergePoints.events.mat']),'MergePoints');
+sidx = find(startsWith(MergePoints.foldernames,"Spont"));
+sponttimes = [MergePoints.timestamps(sidx(1),1) MergePoints.timestamps(sidx(end),2)];
+
+
+maxtimejump = 1; %s
+pupilcycle.amp = NanPadJumps( pupilcycle.timestamps,pupilcycle.amp,maxtimejump );
+pupilcycle.phase = NanPadJumps( pupilcycle.timestamps,pupilcycle.phase,maxtimejump );
+
+%% When do whisks happen with respect to UP/DOWN transitions
+
+[ccg,t] = CCG({EMGwhisk.ints.Wh(:,1),EMGwhisk.ints.Wh(:,2),...
+    SlowWaves.ints.UP(:,1),SlowWaves.ints.UP(:,2)},...
+    [],'norm','rate','binSize',0.02 );
+
+%%
+
+figure
+subplot(2,2,1)
+plot(t,ccg(:,3,1),'LineWidth',2)
+hold on
+plot(t,ccg(:,3,2),'LineWidth',2)
+plot([0 0],[0 0.35],'k')
+xlabel('t lag (s) - DOWN->UP');ylabel('Rate (Wh/s)')
+legend('Whisk Onsets','Wh Offsets')
+
+subplot(2,2,2)
+plot(t,ccg(:,4,1),'LineWidth',2)
+hold on
+plot(t,ccg(:,4,2),'LineWidth',2)
+plot([0 0],[0 0.35],'k')
+xlabel('t lag (s) - UP->DOWN');ylabel('Rate (Wh/s)')
+legend('Whisk Onsets','Wh Offsets')
 %% UP/DOWN durations
 UPDOWN = {'UP','DOWN'};
 UDcolor = {'r','b'};
 for uu = 1:2
     SlowWaves.durs.(UPDOWN{uu}) = diff(SlowWaves.ints.(UPDOWN{uu}),1,2);
+    SlowWaves.durs_np1.(UPDOWN{uu}) = [SlowWaves.durs.(UPDOWN{uu})(2:end);nan];
     SlowWaves.midpoints.(UPDOWN{uu}) = mean(SlowWaves.ints.(UPDOWN{uu}),2);
     SlowWaves.phase.(UPDOWN{uu}) = interp1(pupilcycle.timestamps,pupilcycle.phase,...
         SlowWaves.midpoints.(UPDOWN{uu}),'nearest');
     SlowWaves.amp.(UPDOWN{uu}) = interp1(pupilcycle.timestamps,log10(pupilcycle.amp),...
         SlowWaves.midpoints.(UPDOWN{uu}),'nearest');
-    SlowWaves.highpupil.(UPDOWN{uu}) = SlowWaves.amp.(UPDOWN{uu})>pupilcycle.pupthresh;
+    SlowWaves.hipup.(UPDOWN{uu}) = SlowWaves.amp.(UPDOWN{uu})>pupilcycle.pupthresh;
+    SlowWaves.lopup.(UPDOWN{uu}) = ~SlowWaves.hipup.(UPDOWN{uu});
     
-    SlowWaves.meanEMG.(UPDOWN{uu}) = zeros(size(SlowWaves.midpoints.(UPDOWN{uu})));
-    for dd = 1:length(SlowWaves.ints.(UPDOWN{uu})(:,1))
-        dd
-        EMGin = EMGwhisk.timestamps>SlowWaves.ints.(UPDOWN{uu})(dd,1) & EMGwhisk.timestamps<SlowWaves.ints.(UPDOWN{uu})(dd,2);
-        SlowWaves.meanEMG.(UPDOWN{uu})(dd) = mean(EMGwhisk.EMGenvelope(EMGin));
+    [~,SlowWaves.nwh.(UPDOWN{uu})] = ExcludeIntervals(SlowWaves.ints.(UPDOWN{uu}),EMGwhisk.ints.Wh);
+    SlowWaves.wh.(UPDOWN{uu}) = ~SlowWaves.nwh.(UPDOWN{uu});
+    [~,SlowWaves.nwh_doubles.(UPDOWN{uu})] = ExcludeIntervals([[SlowWaves.ints.(UPDOWN{uu})(1:end-1,1);nan],...
+        [SlowWaves.ints.(UPDOWN{uu})(2:end,2);nan]],EMGwhisk.ints.Wh);
+%     SlowWaves.meanEMG.(UPDOWN{uu}) = zeros(size(SlowWaves.midpoints.(UPDOWN{uu})));
+%     for dd = 1:length(SlowWaves.ints.(UPDOWN{uu})(:,1))
+%         dd
+%         EMGin = EMGwhisk.timestamps>SlowWaves.ints.(UPDOWN{uu})(dd,1) & EMGwhisk.timestamps<SlowWaves.ints.(UPDOWN{uu})(dd,2);
+%         SlowWaves.meanEMG.(UPDOWN{uu})(dd) = mean(EMGwhisk.EMGenvelope(EMGin));
+%     end
+end
+
+
+%% Histograms
+WHNWH = {'wh','nwh'};
+HILO = {'hipup','lopup'};
+
+for uu = 1:2
+    for ww = 1:2
+        for pp = 1:2
+            incondition = SlowWaves.(HILO{pp}).(UPDOWN{uu}) & ...
+                SlowWaves.(WHNWH{ww}).(UPDOWN{uu});
+            [ConditionalUPDOWN.(UPDOWN{uu}).(HILO{pp}).(WHNWH{ww})] = ...
+                ConditionalHist(SlowWaves.phase.(UPDOWN{uu})(incondition),...
+                log10(SlowWaves.durs.(UPDOWN{uu})(incondition)),...
+                'Xbounds',[-pi pi],'Ybounds',[-1.5 1.5],'minX',5,'numXbins',20);
+        end
     end
 end
 
 
+%% Figure: durations conditioned on pupil cycle
+UDcmap = {makeColorMap([1 1 1],[0.8 0 0]),makeColorMap([1 1 1],[0 0 0.8])};
+
+cosx = linspace(-pi,3*pi,100);
+cospamp = [0.6 0.05];
+figure
+for uu = 1:2; for ww = 1:2; for pp = 1:2
+    subplot(4,4,4*(uu-1)+pp+2*(ww-1))
+    colormap(gca,UDcmap{uu})
+        imagesc(ConditionalUPDOWN.(UPDOWN{uu}).(HILO{pp}).(WHNWH{ww}).Xbins,...
+            ConditionalUPDOWN.(UPDOWN{uu}).(HILO{pp}).(WHNWH{ww}).Ybins,...
+            ConditionalUPDOWN.(UPDOWN{uu}).(HILO{pp}).(WHNWH{ww}).pYX')
+        hold on; axis xy; box off
+        imagesc(ConditionalUPDOWN.(UPDOWN{uu}).(HILO{pp}).(WHNWH{ww}).Xbins+2*pi,...
+            ConditionalUPDOWN.(UPDOWN{uu}).(HILO{pp}).(WHNWH{ww}).Ybins,...
+            ConditionalUPDOWN.(UPDOWN{uu}).(HILO{pp}).(WHNWH{ww}).pYX')
+        plot(cosx,(cos(cosx)+1).*cospamp(pp)-1.5,'k')
+        LogScale('y',10)
+         xlim([-pi 3*pi])
+         caxis([0 0.15])
+         %colorbar
+%         if cc == 1 & uu ==1
+%             switch ww
+%                 case 1; ylabel('Non-Whisk')
+%                 case 2; ylabel('Whisky')
+%         elseif cc == 3 & uu ==1
+%             ylabel('Whisky')
+%         end
+        
+        
+    incondition = SlowWaves.(HILO{pp}).(UPDOWN{uu}) & ...
+        SlowWaves.(WHNWH{ww}).(UPDOWN{uu});
+    
+    subplot(4,4,4*(pp-1)+uu+2*(ww-1)+8)
+        plot(SlowWaves.phase.(UPDOWN{uu})(incondition),...
+            log10(SlowWaves.durs.(UPDOWN{uu})(incondition)),...
+            '.','color',UDcolor{uu})
+        hold on
+        plot(SlowWaves.phase.(UPDOWN{uu})(incondition)+2*pi,...
+            log10(SlowWaves.durs.(UPDOWN{uu})(incondition)),...
+            '.','color',UDcolor{uu})
+        plot(cosx,(cos(cosx)+1).*cospamp(pp)-1.8,'k')
+        xlim([-pi 3*pi]);ylim([-2 1.5])
+        LogScale('y',10)
+        
+end;end;end
 %% Figure: UP/DOWN duration return maps
 figure
 for uu = 1:2
     subplot(2,2,uu)
-        plot(log10(SlowWaves.durs.(UPDOWN{uu})(1:end-1)),...
-            log10(SlowWaves.durs.(UPDOWN{uu})(2:end)),...
+        plot(log10(SlowWaves.durs.(UPDOWN{uu})(SlowWaves.nwh_doubles.(UPDOWN{uu}))),...
+            log10(SlowWaves.durs_np1.(UPDOWN{uu})(SlowWaves.nwh_doubles.(UPDOWN{uu}))),...
             '.','color',UDcolor{uu})
+        
         xlim([-2 1.5]);ylim([-2 1.5])
         LogScale('xy',10)
         title(UPDOWN{uu})
         
         
-    subplot(2,2,uu+2)
-        plot(log10(SlowWaves.meanEMG.(UPDOWN{uu})),...
-            log10(SlowWaves.durs.(UPDOWN{uu})),...
-            '.','color',UDcolor{uu})
-        xlim([-2 1.5]);%ylim([-2 1.5])
-        LogScale('xy',10)
-        title(UPDOWN{uu})
-        ylabel('Dur (s)');xlabel('mean EMG')
+%     subplot(2,2,uu+2)
+%         plot(log10(SlowWaves.meanEMG.(UPDOWN{uu})),...
+%             log10(SlowWaves.durs.(UPDOWN{uu})),...
+%             '.','color',UDcolor{uu})
+%         xlim([-2 1.5]);%ylim([-2 1.5])
+%         LogScale('xy',10)
+%         title(UPDOWN{uu})
+%         ylabel('Dur (s)');xlabel('mean EMG')
         
       
 end
 %%
+cosx = linspace(-pi,3*pi,100);
+cosmag = [1 0.1 1 0.1];
+
 figure
 for uu = 1:2
-    subplot(2,2,uu)
-        plot(SlowWaves.phase.(UPDOWN{uu})(SlowWaves.highpupil.(UPDOWN{uu})),...
-            log10(SlowWaves.durs.(UPDOWN{uu})(SlowWaves.highpupil.(UPDOWN{uu}))),...
+    
+    conditions = {[SlowWaves.hipup.(UPDOWN{uu}) & SlowWaves.nwh.(UPDOWN{uu})],...
+        [~SlowWaves.hipup.(UPDOWN{uu}) & SlowWaves.nwh.(UPDOWN{uu})],...
+        [SlowWaves.hipup.(UPDOWN{uu}) & ~SlowWaves.nwh.(UPDOWN{uu})],...
+        [~SlowWaves.hipup.(UPDOWN{uu}) & ~SlowWaves.nwh.(UPDOWN{uu})]};
+    
+    for cc = 1:length(conditions)
+    subplot(4,4,uu+2*(cc-1))
+        plot(SlowWaves.phase.(UPDOWN{uu})(conditions{cc}),...
+            log10(SlowWaves.durs.(UPDOWN{uu})(conditions{cc})),...
             '.','color',UDcolor{uu})
         hold on
-        plot(2*pi+SlowWaves.phase.(UPDOWN{uu})(SlowWaves.highpupil.(UPDOWN{uu})),...
-            log10(SlowWaves.durs.(UPDOWN{uu})(SlowWaves.highpupil.(UPDOWN{uu}))),...
+        plot(2*pi+SlowWaves.phase.(UPDOWN{uu})(conditions{cc}),...
+            log10(SlowWaves.durs.(UPDOWN{uu})(conditions{cc})),...
             '.','color',UDcolor{uu})
+        plot(cosx,cos(cosx).*cosmag(cc)-1,'k')
         xlim([-pi 3*pi]);ylim([-2 1.5])
         LogScale('y',10)
         title(UPDOWN{uu})
-        
-    subplot(2,2,uu+2)
-        plot(SlowWaves.phase.(UPDOWN{uu})(~SlowWaves.highpupil.(UPDOWN{uu})),...
-            log10(SlowWaves.durs.(UPDOWN{uu})(~SlowWaves.highpupil.(UPDOWN{uu}))),...
-            '.','color',UDcolor{uu})
-        hold on
-        plot(2*pi+SlowWaves.phase.(UPDOWN{uu})(~SlowWaves.highpupil.(UPDOWN{uu})),...
-            log10(SlowWaves.durs.(UPDOWN{uu})(~SlowWaves.highpupil.(UPDOWN{uu}))),...
-            '.','color',UDcolor{uu})
-        xlim([-pi 3*pi]);ylim([-2 1.5])
-        LogScale('y',10)
-        title(UPDOWN{uu})  
+        if cc == 1 & uu ==1
+            ylabel('Non-Whisk')
+        elseif cc == 3 & uu ==1
+            ylabel('Whisky')
+        end
+    end
 end
 
 
