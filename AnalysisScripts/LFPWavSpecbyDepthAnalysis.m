1ad480ebbc7758a9cb8cf35231ea65755faa0177
dlevenstein <dan.levenstein@gmail.com>
2019-05-31 19:19:44 -0400

    removed spec corr for speed

--- AnalysisScripts/LFPWavSpecbyDepthAnalysis.m
+++ AnalysisScripts/LFPWavSpecbyDepthAnalysis.m
@@ -1,4 +1,4 @@
-function [ SPECdepth,OSCdepth,speccorr] = LFPWavSpecbyDepthAnalysis(basePath,figfolder)
+function [ SPECdepth,OSCdepth] = LFPWavSpecbyDepthAnalysis(basePath,figfolder)
 % Date XX/XX/20XX
 %
 %Question: 
@@ -196,52 +196,52 @@ end
 
 
 %% Correlation with pupil
- speccorr.freqs = spec.freqs;
- speccorr.channels = spec.channels;
-for cc = 1:length(spec.channels)
-    specvarcorr = bz_LFPSpecToExternalVar( spec.data(:,:,cc),...
-        log10(spec.pup),'specparms','input',...
-        'figparms',[],'numvarbins',20,'varlim',[-0.25 0.25]);
-    speccorr.pup(:,cc)  = specvarcorr.corr;
-    
-    specvarcorr = bz_LFPSpecToExternalVar( spec.data(:,:,cc),...
-        log10(spec.EMG),'specparms','input',...
-        'figparms',[],'numvarbins',20,'varlim',[-0.25 0.25]);
-    speccorr.EMG(:,cc)  = specvarcorr.corr;
-    
-    
-end
-%close all
-spec = rmfield(spec,'data');
-% Interpolate PSS to normalized depth
-speccorr.interpdepth = linspace(-1,0,100);
-speccorr.pupinterp = interp1(CTXdepth',speccorr.pup',speccorr.interpdepth')';
-speccorr.EMGinterp = interp1(CTXdepth',speccorr.EMG',speccorr.interpdepth')';
-%% Figure: Correlation
-figure
-
-subplot(2,2,1)
-imagesc(log10(speccorr.freqs),speccorr.interpdepth,speccorr.pupinterp')
-hold on
-plot(log10(speccorr.freqs([1 end])),-depthinfo.boundaries'*[1 1],'k')
-crameri vik
-LogScale('x',10)
-axis xy
-ColorbarWithAxis([-0.4 0.4],'Pupil Corr.')
-%clim([-0.35 0.35])
-xlabel('f (Hz)');ylabel('Depth')
-
-subplot(2,2,2)
-imagesc(log10(speccorr.freqs),speccorr.interpdepth,speccorr.EMGinterp')
-hold on
-plot(log10(speccorr.freqs([1 end])),-depthinfo.boundaries'*[1 1],'k')
-LogScale('x',10)
-axis xy
-ColorbarWithAxis([-0.4 0.4],'EMG Corr.')
-crameri vik
-xlabel('f (Hz)');ylabel('Depth')
-
-NiceSave('DepthFreqCorr',figfolder,baseName)
+%  speccorr.freqs = spec.freqs;
+%  speccorr.channels = spec.channels;
+% for cc = 1:length(spec.channels)
+%     specvarcorr = bz_LFPSpecToExternalVar( spec.data(:,:,cc),...
+%         log10(spec.pup),'specparms','input',...
+%         'figparms',[],'numvarbins',20,'varlim',[-0.25 0.25]);
+%     speccorr.pup(:,cc)  = specvarcorr.corr;
+%     
+%     specvarcorr = bz_LFPSpecToExternalVar( spec.data(:,:,cc),...
+%         log10(spec.EMG),'specparms','input',...
+%         'figparms',[],'numvarbins',20,'varlim',[-0.25 0.25]);
+%     speccorr.EMG(:,cc)  = specvarcorr.corr;
+%     
+%     
+% end
+% %close all
+% spec = rmfield(spec,'data');
+% % Interpolate PSS to normalized depth
+% speccorr.interpdepth = linspace(-1,0,100);
+% speccorr.pupinterp = interp1(CTXdepth',speccorr.pup',speccorr.interpdepth')';
+% speccorr.EMGinterp = interp1(CTXdepth',speccorr.EMG',speccorr.interpdepth')';
+% %% Figure: Correlation
+% figure
+% 
+% subplot(2,2,1)
+% imagesc(log10(speccorr.freqs),speccorr.interpdepth,speccorr.pupinterp')
+% hold on
+% plot(log10(speccorr.freqs([1 end])),-depthinfo.boundaries'*[1 1],'k')
+% crameri vik
+% LogScale('x',10)
+% axis xy
+% ColorbarWithAxis([-0.4 0.4],'Pupil Corr.')
+% %clim([-0.35 0.35])
+% xlabel('f (Hz)');ylabel('Depth')
+% 
+% subplot(2,2,2)
+% imagesc(log10(speccorr.freqs),speccorr.interpdepth,speccorr.EMGinterp')
+% hold on
+% plot(log10(speccorr.freqs([1 end])),-depthinfo.boundaries'*[1 1],'k')
+% LogScale('x',10)
+% axis xy
+% ColorbarWithAxis([-0.4 0.4],'EMG Corr.')
+% crameri vik
+% xlabel('f (Hz)');ylabel('Depth')
+% 
+% NiceSave('DepthFreqCorr',figfolder,baseName)
 
 %%  Mean depth spec by pupil size, phase and whisking
 %prepare for LFPspec....
@@ -315,7 +315,7 @@ for ww = 1:2
         LogScale('y',10)
         
         %ColorbarWithAxis([-2.4 -1.2],'Mean PSS')
-        clim([1.5 4])
+        clim([2 4])
         xlim([-pi 3*pi])
         if dd == 6
         xlabel('Pupil Phase');
@@ -336,7 +336,7 @@ for ww = 1:2
         hold on; axis xy; box off
         LogScale('y',10)
         %ColorbarWithAxis([-2.4 -1.2],'Mean PSS')
-        clim([1.5 4])
+        clim([2 4])
         if dd == 6
         xlabel('Pupil Size');
         end
@@ -386,7 +386,7 @@ subplot(6,4,(dd-1)*4+4)
         hold on; axis xy; box off
         plot(SPECdepth.(LAYERS{dd}).EMG.varbins,SPECdepth.(LAYERS{dd}).EMG.vardist*1000,'w')
         %plot(log10(EMGwhisk.detectorparms.Whthreshold).*[1 1],[0 max(SPECdepth.freqs)],'k--')
-        clim([1.5 4])
+        clim([2 4])
         ylabel('Freq');
         LogScale('y',10)
                 if dd == 6
