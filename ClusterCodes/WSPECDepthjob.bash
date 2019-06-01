fc391274e31ba0be9e04a3108c0de801277de3a0
dlevenstein <dan.levenstein@gmail.com>
2019-06-01 19:14:23 -0400

    upp ram

--- ClusterCodes/WSPECDepthjob.bash
+++ ClusterCodes/WSPECDepthjob.bash
@@ -3,7 +3,7 @@
 #SBATCH --nodes=1
 #SBATCH --tasks-per-node=1
 #SBATCH --time=12:00:00
-#SBATCH --mem-per-cpu=90G
+#SBATCH --mem-per-cpu=128G
 echo $1
 module load matlab/R2018a
 matlab -nodisplay -nodesktop -singleCompThread -r "cd('/gpfs/data/buzsakilab/DL');addPathsDL;bz_RunAnalysis('LFPWavSpecbyDepthAnalysis','$1','basePath',true);exit;"
