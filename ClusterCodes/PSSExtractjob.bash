05f15b99abe27d8bba9418907ea5ba4c1c6741f9
dlevenstein <dan.levenstein@gmail.com>
2019-08-27 21:01:40 -0400

    Update PSSExtractjob.bash

--- ClusterCodes/PSSExtractjob.bash
+++ ClusterCodes/PSSExtractjob.bash
@@ -3,7 +3,7 @@
 #SBATCH --nodes=1
 #SBATCH --tasks-per-node=1
 #SBATCH --time=12:00:00
-#SBATCH --mem-per-cpu=40G
+#SBATCH --mem-per-cpu=32
 echo $1
 module load matlab/R2018a
 matlab -nodisplay -nodesktop -singleCompThread -r "cd('/gpfs/data/buzsakilab/DL');addPathsDL;bz_RunAnalysis('ExtractPSS','$1','basePath',true);exit;"
