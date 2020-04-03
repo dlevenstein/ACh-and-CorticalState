d08e84ca81b048865e62c14a3d13f82418969061
Dan Levenstein <dan.levenstein@gmail.com>
2020-04-03 19:07:04 -0400

    up to 15cyckes

--- AnalysisScripts/LFPWavSpecbyDepthAnalysis.m
+++ AnalysisScripts/LFPWavSpecbyDepthAnalysis.m
@@ -70,7 +70,7 @@ clear spec
 % spec.channels = lfp.channels;
 spec.channels = CTXchans;
 spec.depth = CTXdepth;
-ncycles = 12; %prev 10
+ncycles = 15; %prev 10
 for cc =1:length(spec.channels)
     bz_Counter(cc,length(spec.channels),'Channel')
     %FFT
