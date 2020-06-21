1b8f45634cfbd24f00932fb7c2040c041de77544
Dan Levenstein <dan.levenstein@gmail.com>
2020-06-21 15:01:08 -0400

    try this

--- ClusterCodes/WSPECDepthjob.bash
+++ ClusterCodes/WSPECDepthjob.bash
@@ -3,7 +3,7 @@
 #SBATCH --nodes=1
 #SBATCH --tasks-per-node=1
 #SBATCH --time=36:00:00
-#SBATCH --mem-per-cpu=100G
+#SBATCH --mem-per-cpu=120G %Increased after last fail... should also change PSS to single
 echo $1
 module load matlab/R2018a
 matlab -nodisplay -nodesktop -singleCompThread -r "cd('/gpfs/data/buzsakilab/DL');addPathsDL;bz_RunAnalysis('LFPWavSpecbyDepthAnalysis','$1','basePath',true);exit;"
